060e1434db84847ce16fd8d0529944617ba0e1f9
zviko29 <trishtaurai@gmail.com>
2026-07-11 11:23:04 +0200

    create entry,promptgenerator ,journal files in week2

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+         Journal  theJournal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+        string choice = "";
+
+        Console.WriteLine("Welcome to the Journal Program!");
+
+        while (choice != "5")
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine($"{prompt}");
+                Console.Write("> ");
+                string response = Console.ReadLine();
+
+                string dateText = DateTime.Now.ToShortDateString();
+                Entry newEntry = new Entry(dateText, prompt, response);
+                theJournal.AddEntry(newEntry);
+            }
+            else if (choice == "2")
+            {
+                theJournal.DisplayAll();
+            }
+            else if (choice == "3")
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+                theJournal.LoadFromFile(filename);
+            }
+            else if (choice == "4")
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+                theJournal.SaveToFile(filename);
+            }
+            else if (choice == "5")
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please enter 1-5.");
+            }
+        }
     }
 }
+        
+    
